cbb4083939e839607a3467bcf43a91f137045211
Austin <austin@dringo.io>
2017-09-24 22:14:47 -0700

    Added interaction with button

--- app/src/main/java/com/example/freespeech/MainActivity.java
+++ app/src/main/java/com/example/freespeech/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.freespeech;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Intent intent = new Intent(this, OpenService.class);
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.toggleButton);
+
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    startService(intent);
+                } else {
+                    stopService(intent);
+                }
+            }
+        });
     }
 }
